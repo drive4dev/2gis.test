0757b960b72cb18f83f35a9ff35f5e74d87a9cbc
maksmaggot <maksmaggot@gmail.com>
2019-11-07 09:25:31 +0700

    beautify routes

--- src/v1/app/Routes.php
+++ src/v1/app/Routes.php
@@ -7,18 +7,19 @@ use Slim\App;
 
 $app->group('/api/v1', function (App $app) {
 
-    $app->get('/building/{id:[0-9]+}/companies', BuildingController::class . ':companies');
-
-    $app->get('/category/{id:[0-9]+}/companies', CategoryController::class . ':companies');
-
-    // http://2gis.test/api/v1/company/radius?lat=85&lon=-25&rad=7000
-    $app->get('/company/radius', CompanyController::class . ':radius');
 
     $app->get('/building', BuildingController::class . ':index');
 
+    $app->get('/building/{id:[0-9]+}/companies', BuildingController::class . ':companies');
+
+
     $app->get('/company/{id:[0-9]+}', CompanyController::class . ':getOne');
+
+    $app->get('/company/radius', CompanyController::class . ':radius');
 
     $app->get('/category', CategoryController::class . ':index');
 
     $app->get('/category/{id:[0-9]+}', CategoryController::class . ':getOne');
+
+    $app->get('/category/{id:[0-9]+}/companies', CategoryController::class . ':companies');
 });
